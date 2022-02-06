12778b1e42a5d0f3f7a51baa1efbb447f91a1511
Ayşenur Erdoğan <aysenurerdoganx@gmail.com>
2022-02-07 00:34:51 +0300

    config file added

---
+++ config-template.php
@@ -0,0 +1,12 @@
+<?php
+
+    /* DATABASE CONFIGS */
+    $dbHost = "localhost";
+    $dbName = "";
+    $dbUserName = "";
+    $dbPass = "";
+    
+    /* INDEX CONFIGS */
+    $mainpage = "https://example.com";
+
+?>
